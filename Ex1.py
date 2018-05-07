38bb84fa6cd0dc51b35b6cef2022c50c000575cb
DarkWizard <nayeemaquib@bennington.edu>
2018-05-07 03:21:29 -0400

    Adding the exercise files

---
+++ Ex1.py
@@ -0,0 +1,40 @@
+# Problem 1
+
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_two_numbers(num1, num2):
+    return num1 - num2
+
+def multiply(num, times):
+    res = num
+    for i in range(1, times):
+        res += num
+    return res
+
+def divide(num, times):
+    res = 0
+    while num > 0:
+        num -= times
+        if num >= 0:
+            res += 1
+    return res
+
+# Problem 2
+
+def calc_ascii_val_of_string(string):
+    asciiDict = {chr(i): i for i in range(129)}
+    sum = 0
+    for i in string:
+        sum += asciiDict[i]
+    return sum
+
+# Problem 3
+
+def length_guesser(my_string, my_length_guess):
+    if len(my_string) == my_length_guess:
+        return 0
+    if len(my_string) > my_length_guess:
+        return 1
+    if len(my_string) < my_length_guess:
+        return -1
---
+++ test_ex1.py
@@ -0,0 +1,34 @@
+import Ex1
+
+def test_add_two_numbers():
+    result = Ex1.add_two_numbers(10, 90)
+    assert result == 100
+
+def test_subtract_two_numbers():
+    result = Ex1.subtract_two_numbers(5, 4)
+    assert result == 1
+
+def test_multiply():
+    result = Ex1.multiply(4, 5)
+    assert result == 20
+
+def test_divide():
+    result1 = Ex1.divide(30, 6)
+    result2 = Ex1.divide(31, 6)
+    assert result1 == 5
+    assert result2 == 5
+
+def test_calc_ascii_val_of_string():
+    result1 = Ex1.calc_ascii_val_of_string('ZZZ')
+    result2 = Ex1.calc_ascii_val_of_string('ZZZ ZZZ')
+    assert result1 == 270
+    assert result2 == 572
+
+def test_length_guesser():
+    result1 = Ex1.length_guesser('Dickinson Catlab', 16)
+    result2 = Ex1.length_guesser('Dickinson Catlab', 8)
+    result3 = Ex1.length_guesser('Dickinson Catlab', 32)
+    assert result1 == 0
+    assert result2 == 1
+    assert result3 == -1
+
